ba3ba28ab3ee7eccf71eca78a4613ad12fae2179
Christian <christian.j.dimitrii@gmail.com>
2021-09-25 22:57:00 +0200

    admin dashboard side bar style in progress

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,65 +1,71 @@
 <template>
-  <div id="admin">
-    <div class="sidebar-page">
+  <div id="admin" style="height: 100vh">
+    <div class="sidebar-page" style="height: 100%">
       <section class="sidebar-layout">
         <b-sidebar
+          :fullheight="fullheight"
           position="static"
           :mobile="mobile"
           :expand-on-hover="expandOnHover"
           :reduce="reduce"
           :delay="expandWithDelay ? 500 : null"
-          type="is-light"
+          type="is-dark"
           v-model="open"
         >
           <div class="p-1">
-            <div class="block">
-              <img
-                src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
-                alt="Lightweight UI components for Vue.js based on Bulma"
-              />
-              <div class="close-icon is-clickable" @click="open = false">
-                <i class="fas fa-times"></i>
+            <div class="columns pt-3">
+              <div class="column">
+                <p class="subtitle is-6 has-text-grey pl-3 is-uppercase">
+                  Vue Shop
+                </p>
+              </div>
+              <div class="column">
+                <span
+                  class="
+                    close-icon
+                    is-clickable is-pulled-right
+                    pr-3
+                    has-text-grey
+                  "
+                  @click="open = false"
+                >
+                  <i class="fas fa-times"></i>
+                </span>
               </div>
             </div>
+
+            <hr class="mx-2 mt-0" style="background-color: #7a7a7a" />
+
+            <div class="admin-img is-flex is-justify-content-center">
+              <figure class="image is-80x80">
+                <img
+                  class="is-rounded"
+                  src="https://bulma.io/images/placeholders/128x128.png"
+                />
+              </figure>
+            </div>
+            <div class="is-flex is-justify-content-center pt-5">
+              <p class="subtitle is-6 pr-1 has-text-success">●</p>
+              <p class="subtitle is-6 has-text-grey">Christian Dimitri</p>
+            </div>
+
+            <hr class="mx-2 mt-0" style="background-color: #7a7a7a" />
+            <b-field class="px-2" message="What do you want to search?">
+              <b-input placeholder="Search..." type="search" icon="search">
+              </b-input>
+              <p class="control">
+                <b-button type="is-dark" icon-left="search" />
+              </p>
+            </b-field>
+
+            <hr class="mx-2 mt-0" style="background-color: #7a7a7a" />
+
             <b-menu class="is-custom-mobile">
-              <b-menu-list label="Admin">
-                <b-menu-item
-                  icon="information-outline"
-                  label="Dashboard"
-                ></b-menu-item>
-                <b-menu-item
-                  active
-                  expanded
-                  icon="settings"
-                  label="Administrator"
-                >
-                  <b-menu-item icon="account" label="Users"></b-menu-item>
-                  <b-menu-item
-                    icon="cellphone-link"
-                    label="Devices"
-                  ></b-menu-item>
-                  <b-menu-item
-                    icon="cash-multiple"
-                    label="Payments"
-                    disabled
-                  ></b-menu-item>
-                </b-menu-item>
-                <b-menu-item icon="account" label="My Account">
-                  <b-menu-item
-                    icon="account-box"
-                    label="Account data"
-                  ></b-menu-item>
-                  <b-menu-item
-                    icon="home-account"
-                    label="Addresses"
-                  ></b-menu-item>
-                </b-menu-item>
-              </b-menu-list>
-              <b-menu-list>
-                <b-menu-item label="Expo" icon="link"></b-menu-item>
-              </b-menu-list>
-              <b-menu-list label="Actions">
-                <b-menu-item icon="logout" label="Logout"></b-menu-item>
+              <b-menu-list label="Menu">
+                <b-menu-item icon="chart-line" label="Overview"></b-menu-item>
+                <b-menu-item label="Products" icon="boxes"></b-menu-item>
+                <b-menu-item label="Orders" icon="shopping-cart"></b-menu-item>
+                <b-menu-item icon="power-off" label="Logout"></b-menu-item>
               </b-menu-list>
             </b-menu>
           </div>
@@ -73,6 +79,10 @@
             </button>
           </transition>
         </div>
+        <div class="container">
+        <p class="title pt-6">Sidebar template</p>
+        <p class="">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+        </div>
       </section>
     </div>
   </div>
@@ -83,6 +93,7 @@ export default {
   name: "Admin",
   data() {
     return {
+      fullheight: true,
       open: true,
       expandOnHover: false,
       expandWithDelay: false,
@@ -94,13 +105,12 @@ export default {
 </script>
 
 <style lang="scss">
-
 .slide-fade-enter-active {
-  transition: all .3s ease;
+  transition: all 0.3s ease;
 }
 
 .slide-fade-leave-active {
-  transition: all .2s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+  transition: all 0.2s cubic-bezier(1, 0.5, 0.8, 1);
 }
 
 .slide-fade-enter {
@@ -112,7 +122,6 @@ export default {
   transform: translateX(-10px);
   opacity: 0;
 }
-
 
 .p-1 {
   padding: 1em;
@@ -198,5 +207,12 @@ export default {
     overflow: hidden;
     text-overflow: ellipsis;
   }
+}
+.menu-list a.is-active {
+  color: dimgrey !important;
+  background-color: whitesmoke;
+}
+.menu-list a.icon-text {
+  color: #7a7a7a;
 }
 </style>
